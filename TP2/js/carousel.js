92a3d7a388e85340e1dac16215436504e4204d48
Maria Ines <contimarines@hotmail.com>
2023-10-03 00:03:19 -0300

    fixed big carousel

---
+++ TP2/js/carousel.js
@@ -0,0 +1,6 @@
+/*"use strict";
+let btn1 = document.querySelector("#btn1").addEventListener("click", loadImg);
+
+function loadImg(){
+    document.querySelector("#imgBig1").classList.add(".changeimg1");
+}*/
