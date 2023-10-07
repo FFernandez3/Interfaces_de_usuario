c8eb75522536ccea9add3d7354be02086db17312
Florencia Fernandez <florenciafernandez0301@gmail.com>
2023-10-07 04:20:06 -0300

    cart and nav styles added

--- TP2/js/nav.js
+++ TP2/js/nav.js
@@ -1,14 +1,36 @@
 "use strict";
 let menuBtn = document.querySelector("#menu-btn");
-let vertNav = document.querySelector("#main-nav");
+let mainNav = document.querySelector("#main-nav");
+let crossNavBtn=document.querySelector("#crossNav-btn");
+
 menuBtn.addEventListener("click", () => {
     console.log("entre");
-    vertNav.classList.toggle("displayNone");
+    mainNav.classList.remove("displayNone");
 }); 
+
+crossNavBtn.addEventListener("click", ()=>{
+    mainNav.classList.add("displayNone");
+});
 
 let avatarMenuBtn = document.querySelector("#avatar-btn");
 let userNav = document.querySelector("#user-nav");
+let crossUserNavBtn=document.querySelector("#crossUserNav-btn");
 avatarMenuBtn.addEventListener("click", () => {
     console.log("entre");
-    userNav.classList.toggle("displayNone");
+    userNav.classList.remove("displayNone");
 }); 
+
+crossUserNavBtn.addEventListener("click", ()=>{
+    userNav.classList.add("displayNone")
+})
+
+
+/*cart */
+let cartBtn=document.querySelector("#cart-btn");
+let crossCart=document.querySelector("#userNavCross")
+cartBtn.addEventListener("click", ()=>{
+    document.querySelector("#cartDiv").classList.remove("displayNone");
+})
+crossCart.addEventListener("click", ()=>{
+    document.querySelector("#cartDiv").classList.add("displayNone");
+})
