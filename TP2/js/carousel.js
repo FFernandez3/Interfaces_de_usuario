f42f31107802f043e65b3905d35a1a630b8aa5b2
Florencia Fernandez <florenciafernandez0301@gmail.com>
2023-10-08 05:27:00 -0300

    my games carousel style change

--- TP2/js/carousel.js
+++ TP2/js/carousel.js
@@ -1,6 +1,49 @@
-/*"use strict";
-let btn1 = document.querySelector("#btn1").addEventListener("click", loadImg);
+"use strict";
+/*addEventListener("DOMContentLoaded", (e) => {
+    const carrusel = document.querySelector(".containerCarrusel");
 
-function loadImg(){
-    document.querySelector("#imgBig1").classList.add(".changeimg1");
-}*/
+    const btnScrollRight = document.querySelector("#btnScrollRight");
+    const btnScrollLeft = document.querySelector("#btnScrollLeft");
+
+    btnScrollLeft.addEventListener("click", ()=>{
+
+                
+                carrusel.scrollLeft -= (carrusel.offsetWidth - 383);
+            
+            console.log("entro a btn izq");
+        }
+    );
+    btnScrollRight.addEventListener("click", ()=>{
+        
+            
+                
+                carrusel.scrollLeft += (carrusel.offsetWidth - 383);
+            
+            console.log("entro a btn derecho");
+        }
+    )
+});*/
+addEventListener("DOMContentLoaded", (e) => {
+const row = document.querySelectorAll(".contCards");
+
+const btnScrollRight = document.querySelectorAll("#btnScrollRight");
+const btnScrollLeft = document.querySelectorAll("#btnScrollLeft");
+
+for(let i = 0; i < btnScrollRight.length;i++){
+    btnScrollRight[i].addEventListener("click", function(){
+        const carrusel = row[i];
+        carrusel.scrollLeft += (carrusel.offsetWidth - 383);
+        console.log("entro al evento click der")
+    });
+    console.log("entro al for derecho")
+}
+
+for(let i = 0; i < btnScrollLeft.length;i++){
+    btnScrollLeft[i].addEventListener("click", function() {
+        const carrusel = row[i];
+        carrusel.scrollLeft -= (carrusel.offsetWidth - 383);
+        console.log("entro al evento click izq")
+    });
+    console.log("entro al for izq")
+}
+});
