d83367520e13c0a17c32cac9e73914b894427422
Florencia Fernandez <florenciafernandez0301@gmail.com>
2023-10-08 22:25:33 -0300

    successful register animation added

--- TP2/js/registerAnimation.js
+++ TP2/js/registerAnimation.js
@@ -1,1 +1,19 @@
 
+btn=document.querySelector('#signUp-btn').addEventListener("click", register)
+
+function register() {
+    // Simula un registro exitoso
+    // En un caso real, aquí tendrías tu lógica de registro y validaciones
+    const overlay=document.querySelector('#overlay');
+      const successMessage = document.getElementById('successMessage');
+      successMessage.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+
+      // Agrega el temporizador
+      const timer = setInterval(() => {
+        successMessage.classList.add('hidden');
+        overlay.classList.add('hidden');
+        clearInterval(timer);
+      }, 3000);
+    
+  }
