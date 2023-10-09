5fe8e439547b81e9d389bb8f7fc22531e5fa012e
Maria Ines <contimarines@hotmail.com>
2023-10-08 23:35:43 -0300

    mg carousel animation added

--- TP2/js/carousel.js
+++ TP2/js/carousel.js
@@ -25,7 +25,7 @@
 });*/
 addEventListener("DOMContentLoaded", (e) => {
 const row = document.querySelectorAll(".contCards");
-
+const cards = document.querySelectorAll(".mgCard");
 const btnScrollRight = document.querySelectorAll("#btnScrollRight");
 const btnScrollLeft = document.querySelectorAll("#btnScrollLeft");
 
@@ -33,6 +33,14 @@ for(let i = 0; i < btnScrollRight.length;i++){
     btnScrollRight[i].addEventListener("click", function(){
         const carrusel = row[i];
         carrusel.scrollLeft += (carrusel.offsetWidth - 383);
+        for(let i=0; i< cards.length; i++){
+            cards[i].classList.add("skewActive");
+        }
+        setTimeout(() => {
+            cards.forEach(card => {
+                card.classList.remove('skewActive');
+            });
+        }, 300);
         console.log("entro al evento click der")
     });
     console.log("entro al for derecho")
@@ -42,8 +50,21 @@ for(let i = 0; i < btnScrollLeft.length;i++){
     btnScrollLeft[i].addEventListener("click", function() {
         const carrusel = row[i];
         carrusel.scrollLeft -= (carrusel.offsetWidth - 383);
+        for(let i=0; i< cards.length; i++){
+            cards[i].classList.add("skewActive2");
+        }
+        setTimeout(() => {
+            cards.forEach(card => {
+                card.classList.remove('skewActive2');
+            });
+        }, 300);
+
         console.log("entro al evento click izq")
     });
     console.log("entro al for izq")
 }
+
+
 });
+
+
