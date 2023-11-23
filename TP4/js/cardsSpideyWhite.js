b2bb2c5465b46b1f809f55805496946ade19fd39
ariel-003 <arielcou@gmail.com>
2023-11-23 13:51:56 -0300

    transform added to cards ws

---
+++ TP4/js/cardsSpideyWhite.js
@@ -0,0 +1,66 @@
+/*document.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
+    var card1 = document.querySelector('#sw1');
+    var card2 = document.querySelector('#sw2');
+    var card3 = document.querySelector('#sw3');
+    var speed = 0.05; // Ajusta la velocidad según tus necesidades
+  
+    // Mueve el Duende Verde en la dirección del scroll, pero más lentamente
+    card1.style.transform = 'translate(-5%, -21%) translateY(' + scrollPosition * speed + 'px)';
+    card2.style.transform = 'translate(-4%, -20%) translateY(' + scrollPosition * speed + 'px)';
+    card3.style.transform = 'translate(-3%, -19%) translateY(' + scrollPosition * speed + 'px)';
+
+   card1.addEventListener('mouseover', function(){
+    card1.style.transform = 'transform: translateY(10px) rotateX(45deg)';
+   }) 
+  });*/
+  document.addEventListener('DOMContentLoaded', function () {
+    var scrollPosition = window.scrollY;
+    var card1 = document.querySelector('#sw1');
+    var card2 = document.querySelector('#sw2');
+    var card3 = document.querySelector('#sw3');
+    var speed = 0.05; // Ajusta la velocidad según tus necesidades
+
+    function handleScroll() {
+        // Mueve las tarjetas en la dirección del scroll, pero más lentamente
+        card1.style.transform = 'translate(-5%, -21%) translateY(' + scrollPosition * speed + 'px)';
+        card2.style.transform = 'translate(-4%, -20%) translateY(' + scrollPosition * speed + 'px)';
+        card3.style.transform = 'translate(-3%, -19%) translateY(' + scrollPosition * speed + 'px)';
+    }
+
+    function handleMouseEnter(card) {
+        card.style.transform = 'translateY(10px) rotateX(45deg)';
+    }
+
+    function handleMouseLeave(card) {
+        card.style.transform = ''; // Restaurar la transformación original
+    }
+
+    document.addEventListener('scroll', function () {
+        scrollPosition = window.scrollY;
+        handleScroll();
+    });
+
+    card1.addEventListener('mouseenter', function () {
+        handleMouseEnter(card1);
+    });
+    card1.addEventListener('mouseleave', function () {
+        handleMouseLeave(card1);
+    });
+
+    card2.addEventListener('mouseenter', function () {
+        handleMouseEnter(card2);
+    });
+    card2.addEventListener('mouseleave', function () {
+        handleMouseLeave(card2);
+    });
+
+    card3.addEventListener('mouseenter', function () {
+        handleMouseEnter(card3);
+    });
+    card3.addEventListener('mouseleave', function () {
+        handleMouseLeave(card3);
+    });
+});
+
+
