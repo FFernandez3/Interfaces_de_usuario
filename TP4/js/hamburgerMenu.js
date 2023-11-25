e441754734c2cc3359f56c751f2a3404d7917052
ariel-003 <arielcou@gmail.com>
2023-11-24 21:05:07 -0300

    menu added

--- TP4/js/hamburgerMenu.js
+++ TP4/js/hamburgerMenu.js
@@ -1,4 +1,7 @@
 document.getElementById('hamburguerMenu').addEventListener('click', function(event) {
     event.preventDefault();
     this.classList.toggle('hamburguerMenuOpen');
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+    
 });
